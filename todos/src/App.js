26a9d8df5d70ada83052f6804d9463110278c593
bschue <bschuess3@gmail.com>
2018-08-08 11:16:36 -0400

    second

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import todoList from './todos.json';
-// class TodoItem extends Component {
-//   state = {}
-//   singleTodo = () => {
-//     for (todo in todoList){
-//       return todo
-//     }
-//   }
-//   render() {
-//     return( 
-//       this.singleTodo()
-//     )
-//   }
-// }
+
+class TodoItem extends Component {
+  
+  render() {
+    return(
+      <li className={this.props.value}>
+        <div className="view">
+          <input className="toggle" type="checkbox" check={this.props.completed}/>
+          <label>{this.props.TodoItem}</label>
+          <button className="destroy"></button>
+        </div>
+      </li>
+    )
+  }
+}
+class ToDoList extends Component {
+  constructor (props) {
+    super(props);
+    this.state= {
+      todos: todoList
+    }
+  }
+  render() {
+    return (
+    <React.Fragment>
+        <ul className="renderedToDoList">
+          {this.state.todos.map( todo => {
+            if (todo.completed){
+              return(
+              <TodoItem toDoItem={todo.Title} key={todo.id} completed={todo.completed}/>)
+            }else{
+              return(
+                <TodoItem value="" toDoItem={todo.title} key={todo.id} completed={todo.completed}/>
+              )
+            }
+          })}
+        </ul>
+    </React.Fragment>
+    )
+  }
+}
 class App extends Component {
 
 
@@ -22,10 +49,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="todoH1">Todos</h1>
+          <h1 className="todoH1">To-Dos</h1>
+          <input classeName="addTodo" placeholder="Have something to do?" autoFocus/>
         </header>
-        {/* <TodoItem>
-        </TodoItem> */}
+        <ToDoList />
+        <footer className="footer">
+          <span className="todo-count"><strong>0</strong> item(s) left</span>
+          <button className="clear-completed">Clear completed</button>
+        </footer>
       </div>
     );
   }
